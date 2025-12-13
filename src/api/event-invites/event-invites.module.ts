d5fd3e6ae0c6a0bb8333c4ffe09163ba50b57513
NicoCaill13 <n.cailleux13@gmail.com>
2025-12-13 15:06:33 +0100

    feat(event-invite):search users to invite

---
+++ src/api/event-invites/event-invites.module.ts
@@ -0,0 +1,11 @@
+// src/api/event-invites/event-invites.module.ts
+import { Module } from '@nestjs/common';
+import { EventInvitesController } from './event-invites.controller';
+import { EventInvitesService } from './event-invites.service';
+
+@Module({
+  controllers: [EventInvitesController],
+  providers: [EventInvitesService],
+  exports: [EventInvitesService],
+})
+export class EventInvitesModule { }
